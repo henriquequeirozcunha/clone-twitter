4c7f8df77e9910e351337fe61dd73dc22c4d0510
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-03-20 18:46:48 -0300

    feat: add EditButton to ProfilePage

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -8,6 +8,7 @@ import {
   LocationIcon,
   CakeIcon,
   FollowAge,
+  EditButton,
 } from './styles';
 
 const ProfilePage: React.FC = () => {
@@ -18,7 +19,7 @@ const ProfilePage: React.FC = () => {
       </Banner>
 
       <ProfileData>
-        {/* <EditButton outlined>Editar Perfil</EditButton> */}
+        <EditButton outlined>Editar Perfil</EditButton>
 
         <h1>Henrique Cunha</h1>
         <h2>@henriquequeirozcunha</h2>
