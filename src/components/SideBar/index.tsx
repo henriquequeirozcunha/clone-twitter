c978c7c34c45b65aa39f9cd581513b4df7a6fa2b
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-03-24 22:21:50 -0300

    feat: add sidebar to layout

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body,
+} from './styles';
+
+const SideBar: React.FC = () => {
+  return (
+    <Container>
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no Twitter" />
+        <SearchIcon />
+      </SearchWrapper>
+
+      <Body>
+        <p>{'Teste Descripca'.repeat(90)}</p>
+      </Body>
+    </Container>
+  );
+};
+
+export default SideBar;
