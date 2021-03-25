07b288ad43755ff9a1794e098eda84696c5d9266
Henrique Cunha <henrique.cunha@corpus.inf.br>
2021-03-24 22:48:12 -0300

    refactor: add FollowSuggestion to SideBar component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -7,6 +7,8 @@ import {
   SearchIcon,
   Body,
 } from './styles';
+import List from '../List';
+import FollowSuggestion from '../FollowSuggestion';
 
 const SideBar: React.FC = () => {
   return (
@@ -17,7 +19,18 @@ const SideBar: React.FC = () => {
       </SearchWrapper>
 
       <Body>
-        <p>{'Teste Descripca'.repeat(90)}</p>
+        <List
+          title="Sugestões"
+          elements={[
+            <FollowSuggestion name="Henrique" nickname="@henrique" />,
+            <FollowSuggestion name="Flávio" nickname="@flavio" />,
+            <FollowSuggestion name="Lilian" nickname="@lilian" />,
+          ]}
+        />
+        <List
+          title="Sugestões"
+          elements={[<h1>Teste 1</h1>, <h1>Teste 2</h1>, <h1>Teste 3</h1>]}
+        />
       </Body>
     </Container>
   );
